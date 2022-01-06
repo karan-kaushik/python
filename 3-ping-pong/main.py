5f8a84a340d24e0f0e60aa8b9749d7716c7e372f
Karan Kaushik <karankaushik715@gmail.com>
2022-01-07 02:47:58 +0530

    completed ping pong game

--- 3-ping-pong/main.py
+++ 3-ping-pong/main.py
@@ -1,1 +1,51 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Ping Pong")
+screen.tracer(0)
+
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(right_paddle.move_up, "Up")
+screen.onkeypress(right_paddle.move_down, "Down")
+screen.onkeypress(left_paddle.move_up, "w")
+screen.onkeypress(left_paddle.move_down, "s")
+
+game_over = False
+while not game_over:
+    screen.update()
+    time.sleep(ball.move_speed) 
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    if ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.update_l_score()
+
+    if ball.xcor() < -380:
+        ball.reset()
+        scoreboard.update_r_score()
+    
+    if scoreboard.l_score == 5 or scoreboard.r_score == 5:
+        scoreboard.game_over()
+        game_over = True
+
+screen.exitonclick()
